16e67d56b5546192271277af49fecd36ab62cdd2
Alan Bertinat <alanbertinatestudio@gmail.com>
2024-11-04 20:50:49 -0300

    Ultimo, falta configurar y estructurar

--- src/main/java/com/tallermecanico/TallermecanicoApplication.java
+++ src/main/java/com/tallermecanico/TallermecanicoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TallermecanicoApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("Hola Mundo");
 		SpringApplication.run(TallermecanicoApplication.class, args);
 	}
 
